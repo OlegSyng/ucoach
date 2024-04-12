1979a9add4d979fede10cff7b0ccf4209e248c68
Oleg Syngaivskyi <olegsyng@Olegs-MacBook-Pro.local>
2024-04-12 19:49:46 +0300

    middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { getToken } from 'next-auth/jwt'
+import { NextURL } from 'next/dist/server/web/next-url'
+import { NextRequest, NextResponse } from 'next/server'
+import { BASE_URL } from './utils/endpoints'
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.SECRET })
+  if (!token) {
+    const loginUrl = new NextURL('/auth/login', BASE_URL)
+    if (req.nextUrl.pathname !== '/') {
+      loginUrl.searchParams.set('redirect_after', req.nextUrl.toString())
+    }
+    return NextResponse.redirect(loginUrl)
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /auth/ routes
+     * 1. /api/ routes
+     * 2. /_next/ (Next.js internals)
+     * 3. /_proxy/ (special page for OG tags proxying)
+     * 4. /_static (inside /public)
+     * 5. /_vercel (Vercel internals)
+     * 6. Static files (e.g. /favicon.ico, /sitemap.xml, /robots.txt, etc.)
+     */
+    '/((?!api/|auth/|_next/|_proxy/|_static|_vercel|[\\w-]+\\.\\w+).*)',
+  ],
+}
